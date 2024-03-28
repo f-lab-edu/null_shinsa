3d51b709f08e27c564a15cb5e95523efc216325b
mins1031 <urisegea@naver.com>
2024-03-28 20:09:44 +0900

    깃 파일 제거

---
+++ nullshinsa-member/src/main/java/com/example/nullshinsamember/NullshinsaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.nullshinsamember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullshinsaMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullshinsaMemberApplication.class, args);
+	}
+
+}
---
+++ nullshinsa-member/src/test/java/com/example/nullshinsamember/NullshinsaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nullshinsamember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullshinsaMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
